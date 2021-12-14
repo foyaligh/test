3c5037185ab1c1ffcbabec58374f48ec8b1f23c5
foyaligh <michael.foy@lumen.com>
2021-12-13 19:55:04 -0700

    initital checkin 2

--- src/main/java/com/example/test/rest/HelloController.java
+++ src/main/java/com/example/test/rest/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.test.rest;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/api/hello")
 public class HelloController {
 
-    @RequestMapping("$name")
+    @RequestMapping(value = "$name", produces = MediaType.APPLICATION_JSON_VALUE)
     public String sayHello(@PathVariable String name) {
         return "Hello: " + name;
     }
